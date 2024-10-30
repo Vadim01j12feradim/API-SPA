9377f19a323d2db710dd374ae5868d47fd629502
Vadim01j12feradim <izmael.guzman7281@alumnos.udg.mx>
2024-10-29 21:59:39 -0600

    asdsdsadadasdsad

--- index.js
+++ index.js
@@ -3,12 +3,61 @@ const mysql = require('mysql2')
 const cors = require('cors');
 const io = require('socket.io-client');
 
+const CryptoJS = require('crypto-js');
+
+const secretKey = "?G&;3GCK0wB?!n5?;q0q_FQt(Adrqa"; 
+
+const encryptPassword = (password) => {
+  if (!password) {
+    throw new Error("Password cannot be empty");
+  }
+  return CryptoJS.AES.encrypt(password, secretKey).toString();
+};
+
+const decryptPassword = (encryptedPassword) => {
+  if (!encryptedPassword) {
+    throw new Error("Encrypted password cannot be empty");
+  }
+  const bytes = CryptoJS.AES.decrypt(encryptedPassword, secretKey);
+  const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+  
+  if (!originalPassword) {
+    throw new Error("Decryption failed: Invalid encrypted password");
+  }
+  
+  return originalPassword;
+};
+
+
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
 // Connect to the Socket.IO server
-const socket = io('http://localhost:3001');
+const getCurrentFormatedData = () =>{
+    const date = new Date();
+
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based in JS
+        const day = String(date.getDate()).padStart(2, '0');
+        
+        const hours = String(date.getHours()).padStart(2, '0');
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+        const seconds = String(date.getSeconds()).padStart(2, '0');
+        
+        const mysqlDateTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    return mysqlDateTime
+}
+
+
+const socket = io('http://socket:3001');
+// const socket = io('http://localhost:3001');
 
 
 socket.on('connect', () => {
-    console.log('Connected to server');
+    console.log('Connected to socket server');
     
 });
 
@@ -18,8 +67,12 @@ socket.on('chatMessage', (data) => {
 });
 
 // Handle connection errors
-socket.on('connect_error', (error) => {
-    console.error('Connection error:', error);
+socket.on('connect_error', async (error) => {
+    // console.error('Connection error:', error);
+    console.log("Tring socket connection");
+    
+    await sleep(2000)
+    socket.connect();
 });
 
 
@@ -27,14 +80,35 @@ const app = express();
 app.use(cors());
 const port = 3000;
 
-const pool = mysql.createPool({
-    host:'localhost',
-    user:'username',
-    password:'password',
-    database:'SPA',
-    waitForConnections: true,
-    connectionLimit: 40,
-    queueLimit:0
+
+let pool = null;
+
+async function initializeDatabaseConnection() {
+  while (true) {
+    try {
+      pool = mysql.createPool({
+        host: 'db',
+        // host: 'localhost',
+        user: 'username',
+        password: 'password',
+        database: 'SPA',
+        waitForConnections: true,
+        connectionLimit: 40,
+        queueLimit: 0
+      });
+      console.log("Database connection pool created successfully");
+      break;
+    } catch (error) {
+      console.error("Error:", error.message);
+      await sleep(2000);
+    }
+  }
+}
+
+
+
+initializeDatabaseConnection().then(() => {
+    console.log(`Connected`);
 });
 
 
@@ -50,14 +124,29 @@ function executeQuery(query, params = []) {
     });
 }
 
+
+
 app.use(express.json());
 
 
 app.get('/cars/:id', (req, res) => {
     const carId = parseInt(req.params.id, 10); // Extract ID from request parameters
     
-
-    const querySelect = `SELECT * FROM vehicles WHERE id = ? `;
+    
+    const querySelect = `SELECT 
+        vehicles.id, 
+        vehicles.plates,
+        vehicles.brand,
+        vehicles.color,
+        vehicles.model,
+        vehicles.latitude,
+        vehicles.longitude,
+        DATE_FORMAT(vehicles.created_at, '%Y-%m-%d %H:%i:%s') as created_at,
+        DATE_FORMAT(vehicles.updated_at, '%Y-%m-%d %H:%i:%s') as updated_at,
+        vehicles.status,
+        users.username 
+        FROM vehicles INNER JOIN users ON vehicles.user_id = users.id 
+    WHERE vehicles.id = ? and status = 1`;
 
     executeQuery(querySelect, [carId])
     .then(result => {
@@ -82,33 +171,89 @@ app.get('/cars/:id', (req, res) => {
         });
     });
 
-
-    if (car) {
-        res.status(200).json(car); // Respond with the car data
-    } else {
-        res.status(404).json({ message: 'Car not found' }); // Respond with an error if not found
-    }
 });
 
+
+app.put('/carsById', (req,res) => {
+    const newItem = req.body
+ 
+    let querySelect = `
+        SELECT 1 
+        FROM vehicles 
+        WHERE plates = ? 
+          AND id != ?`;
+
+
+          
+          //  executeQuery(querySelect, [newItem.placas, newItem.marca, newItem.color, newItem.modelo,current, newItem.id,newItem.placas,newItem.id])
+          executeQuery(querySelect, [newItem.placas,newItem.id])
+          .then(result => {
+              var code = 300
+              if (result.length == 0) {
+                  code = 200
+                  const current  = getCurrentFormatedData()
+                  newItem.updated_at = current
+                  querySelect = `
+                    UPDATE vehicles 
+                    SET plates = ?, brand = ?, color = ?, model = ?, updated_at = ? 
+                    WHERE id = ?
+                  `
+
+                  executeQuery(querySelect, [newItem.placas, newItem.marca, newItem.color, newItem.modelo,current, newItem.id])
+          .then(result => {
+            code = 300
+            if (result.affectedRows > 0) {
+                code = 200
+                socket.emit("message", newItem)
+            }
+          })
+          .catch(err => {
+              code = 500 
+          });
+
+         }
+ 
+         res.json({
+             message: result == 300 ? "Error":"Ok",
+             data: newItem,
+             code: code
+         });
+         socket.emit("message", newItem)
+     })
+     .catch(err => {
+         code = 500
+         res.json({
+             message: 'Error creating item',
+             error: err,
+             code: code
+         });
+     });
+ })
+
+ 
 
 app.put('/cars', (req,res) => {
    const newItem = req.body
 
-    const querySelect = `UPDATE vehicles SET latitud = ?,  longitud = ?, updated_at = ? WHERE id = ? `;
+    const querySelect = `UPDATE vehicles SET latitude = ?,  longitude = ?, updated_at = ? WHERE id = ? `;
 
-    executeQuery(querySelect, [newItem.latitud, newItem.longitud, newItem.updated_at, newItem.id])
+    executeQuery(querySelect, [newItem.latitude, newItem.longitude, newItem.updated_at, newItem.id])
     .then(result => {
         var code = 300
+        var message = "Not found"
         if (result.affectedRows > 0) {
-            code = 200
-        }
 
+            socket.emit("message", newItem);
+            code = 200
+            message = "Ok"
+     
+        }
+        
         res.json({
-            message: result == 300 ? "Error":"Ok",
+            message: message,
             data: newItem,
             code: code
         });
-        socket.emit("message", newItem)
 
     })
     .catch(err => {
@@ -127,23 +272,70 @@ app.get('/', (req,res) => {
 })
 
 
+
+app.delete('/cars', (req,res) => {
+    const { id } = req.query;
+    var query = "UPDATE vehicles SET status = ? WHERE id = ?"
+   
+    executeQuery(query, [0, id])
+    .then(result => {
+        var code = 300
+
+        if (result.affectedRows > 0) {
+            code = 200
+        }
+
+        res.json({
+            message: result == 300 ? "Not found":"Ok",
+            data: result,
+            code: code
+        });
+
+
+    })
+    .catch(err => {
+        code = 500
+        res.json({
+            message: 'Error creating item',
+            error: err,
+            code: code
+        });
+    });
+})
+
 app.get('/cars', (req,res) => {
     const { role } = req.query;
     const { idUser } = req.query;
-    var query = "SELECT vehicles.*, users.username FROM vehicles INNER JOIN users ON vehicles.user_id = users.id WHERE user_id = ?;"
+    var query = `SELECT  
+        vehicles.id, 
+        vehicles.plates,
+        vehicles.brand,
+        vehicles.color,
+        vehicles.model,
+        vehicles.latitude,
+        vehicles.longitude,
+        DATE_FORMAT(vehicles.created_at, '%Y-%m-%d %H:%i:%s') as created_at,
+        DATE_FORMAT(vehicles.updated_at, '%Y-%m-%d %H:%i:%s') as updated_at,
+        vehicles.status,
+        users.username 
+        FROM vehicles INNER JOIN users ON 
+          vehicles.user_id = users.id  where vehicles.status = 1`
+
     var params = []
 
     switch (role) {
         case "viewer":
             params = [idUser]
+            query += " and user_id = ? "
             break;
-        case "root":
-            query = "SELECT * FROM vehicles"
+        case "admin":
             break;    
         default:
             break;
     }
-   
+
+   query += " ORDER BY vehicles.id DESC"
+
     executeQuery(query, params)
     .then(result => {
         
@@ -167,20 +359,23 @@ app.get('/cars', (req,res) => {
     
 })
 
-app.get('/items', (req,res) => {
-    const { username } = req.query;
-    const { password } = req.query;
-    console.log(username);
-    console.log(password);
-    
-    
-    const querySelect = `SELECT * FROM users WHERE username = ? and password = ?`;
+app.post('/login', (req,res) => {
+    const newItem = req.body
+    const querySelect = `SELECT * FROM users WHERE username = ?`;
 
-    executeQuery(querySelect, [username, password])
+    executeQuery(querySelect, [newItem.username])
     .then(result => {
         var code = 300
         if (result.length > 0) {
-            code = 200
+            const temp  = result[0]
+            const Preal = decryptPassword(temp.password)
+            const pParam = decryptPassword(newItem.password)
+            if (Preal ==  pParam) {
+                code = 200
+            }else{
+                code = 300
+            }
+
         }
 
         res.json({
@@ -206,10 +401,10 @@ app.get('/items', (req,res) => {
 app.post('/cars',(req, res) => {
     const newItem = req.body
     
-    const query = `INSERT INTO vehicles (user_id, placas, marca, color, modelo, latitud, longitud )
+    const query = `INSERT INTO vehicles (user_id, plates, brand, color, model, latitude, longitude )
                     SELECT ?, ?, ?,?, ?, ?, ?
                     WHERE NOT EXISTS (
-                    SELECT 1 FROM vehicles WHERE placas = ?  
+                    SELECT 1 FROM vehicles WHERE plates = ?  
                     );`;
                     
     var code = 200;
@@ -278,9 +473,5 @@ app.post('/items',(req, res) => {
 app.listen(port, () => {
     console.log(`API is running on http://localhost:${port}`)
 })
-
-
-
-
 
 
